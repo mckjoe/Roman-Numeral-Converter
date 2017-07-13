a9951a84fe2f0e55745fe2007790b539415e4508
Evan Johnson and Joe McKinney <joe@>
2017-07-13 11:40:27 -0700

    add core functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,72 @@
 //Business Logic
-var I = 1;
-var V = 5;
-var X = 10;
-var L = 50;
-var C = 100;
-var D = 500;
-var M = 1000;
+var result = '';
 
 function  makeRoman (userInput) {
   if (isNaN(userInput)) {
-    return "Please enter a number!";
+    return false;
   } else if (userInput === 0 || userInput > 3999) {
-    return "No conversion possible!";
-  } else if ()
-
+    return false;
+  } else {
+    return true;
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function doIt (userInput) {
+  while (userInput >= 1000) {
+    result += 'M';
+    userInput -= 1000;
+  } while (userInput >= 900) {
+    result += 'CM';
+    userInput -= 900;
+  } while (userInput >= 500) {
+    result += 'D';
+    userInput -= 500;
+  } while (userInput >= 400) {
+    result += 'CD';
+    userInput -= 400;
+  } while (userInput >= 100) {
+    result += 'C';
+    userInput -= 100;
+  } while (userInput >= 90) {
+    result += 'XC';
+    userInput -= 90;
+  } while (userInput >= 50) {
+    result += 'L';
+    userInput -= 50;
+  } while (userInput >= 40) {
+    result += 'XL';
+    userInput -= 40;
+  } while (userInput >= 10) {
+    result += 'X';
+    userInput -= 10;
+  } while (userInput >= 9) {
+    result += 'IX';
+    userInput -= 9;
+  } while (userInput >= 5) {
+    result += 'V';
+    userInput -= 5;
+  } while (userInput >= 4) {
+    result += 'IV';
+    userInput -= 4;
+  } while (userInput >= 1) {
+    result += 'I';
+    userInput -= 1;
+} return result ;
+}
 
 //User Interface Logic
 $(document).ready(function() {
   $("#formConverter").submit(function(event) {
     var userInput = parseInt($("#numberInput").val());
     var output = makeRoman(userInput);
-    $("p").text(output);
+
+    if (output === true) {
+      doIt(userInput);
+    } else if (output === false){
+      result = "Please enter a valid number";
+    }
+
+    $("p").text(result);
     event.preventDefault();
   });
 });
